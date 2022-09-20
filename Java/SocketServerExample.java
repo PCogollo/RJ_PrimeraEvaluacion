0ff49e84d17218c266ccbaf8c71fe1606cca8676
PCogollo <git@github.com:PCogollo/RJ_PrimeraEvaluacion.git>
2022-09-20 16:53:28 -0500

    Second Server try

--- Java/SocketServerExample.java
+++ Java/SocketServerExample.java
@@ -4,6 +4,7 @@ import java.io.ObjectOutputStream;
 import java.lang.ClassNotFoundException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 /**
 * This class implements java Socket server
@@ -20,7 +21,7 @@ public class SocketServerExample {
 	public static void main(String args[]) throws IOException, ClassNotFoundException{
 		//create the socket server object
 		server = new ServerSocket(port);
-
+		Random random = new Random();
 
 
 		//keep listens indefinitely until receives 'exit' call or program terminates
@@ -32,24 +33,96 @@ public class SocketServerExample {
 			//read from socket to ObjectInputStream object
 			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 
-			//convert ObjectInputStream object to String
-		    String chosenWord = (String) ois.readObject();  
-		    System.out.println("The client's chosen word is: " + chosenWord);
-
 			//create ObjectOutputStream object
 			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 
+			String response = (String) ois.readObject();
+			System.out.println(response);
+
+			if(response.equalsIgnoreCase("yes")){
+				System.out.println("A");
+
+				oos.writeObject("Choose an object: ");
+				System.out.println("B");
+
+				response = (String) ois.readObject();
+				int serverChosenWord = random.nextInt(3) + 1;
+
+				switch (serverChosenWord){
+					case 1:
+						//Server chose Rock
+						if(response.equalsIgnoreCase("Rock"))
+							oos.writeObject("It's a draw: You chose " + response + " and the server chose Rock");
+
+						else if(response.equalsIgnoreCase("Paper"))
+							oos.writeObject("You won: You chose " + response + " and the server chose Rock");
+
+						else if(response.equalsIgnoreCase("Scissors"))
+							oos.writeObject("You lost: You chose " + response + " and the server chose Rock");
+						else
+							oos.writeObject("Invalid option in game");
+					break;
+
+					case 2:
+						//Server chose Paper
+						if(response.equalsIgnoreCase("Rock"))
+							oos.writeObject("You lost: You chose " + response + " and the server chose Paper");
+
+						else if(response.equalsIgnoreCase("Paper"))
+							oos.writeObject("It's a draw: You chose " + response + " and the server chose Paper");
+
+						else if(response.equalsIgnoreCase("Scissors"))
+							oos.writeObject("You won: You chose " + response + " and the server chose Paper");
+						else
+							oos.writeObject("Invalid option in game");
+					break;
+
+					case 3:
+						//Server chose Scissors
+						if(response.equalsIgnoreCase("Rock"))
+							oos.writeObject("You won: You chose " + response + " and the server chose Scissors");
+
+						else if(response.equalsIgnoreCase("Paper"))
+							oos.writeObject("You lost: You chose " + response + " and the server chose Scissors");
+
+						else if(response.equalsIgnoreCase("Scissors"))
+							oos.writeObject("It's a draw: You chose " + response + " and the server chose Scissors");
+						else
+							oos.writeObject("Invalid option in game");
+					break;
+
+				}
+
+			}
+			else if(response.equalsIgnoreCase("no")){
+				continue;
+			}
+			else if(response.equalsIgnoreCase("exit"))
+				break;
+			
+			else{
+				oos.writeObject("Invalid option");
+				continue;
+			}
+
+			/*//convert ObjectInputStream object to String
+		    String chosenWord = (String) ois.readObject();  
+		    System.out.println("Do you want to play Rock Paper Scissors? " + chosenWord);
+
+
 			//write object to Socket
 			oos.writeObject("Your chosen letter was: " + chosenWord);
-
+			*/
 			//close resources
 			ois.close();
 			oos.close();
 			socket.close();
 
 			//terminate the server if client sends exit request
-			if(chosenWord.equalsIgnoreCase("exit")) break;
-		}
+			if(response.equalsIgnoreCase("exit")) break;
+		}	
+				
+		
 		System.out.println("Shutting down Socket server!!");
 		//close the ServerSocket object
 
